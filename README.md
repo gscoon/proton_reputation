689175559e31640b83c5c70bba979997f80f9b47
Gerren Scoon <gscoon@gmail.com>
2017-08-17 01:57:34 -0400

    added comments to new.php. organized files a bit

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 1. Please read the __1-Communicate.md__ file.
 2. Please read the __2-Design.md__ file.
 3. Please run the code in the following files:
-	* __new.php__ => handles a new message
-	* __spamify.php__ => adds a message to spam
-	* __unspamify.php__ => removes a message from spam
+	* __new.php__ - handles a new message
+	* __spamify.php__ - manually adds a message to spam
+	* __unspamify.php__ - manually removes a message from spam
--- new.php
+++ new.php
@@ -5,73 +5,77 @@
 
 	const SPAM_ASSASSIN_THRESHOLD = 4;
 
+	// include 2 remote classes
 	include "inc/db.php";
 	include "inc/email.php";
 
-	// receive email header
+	// read email header from file
 	$emailHeader = file_get_contents('./email_header.txt');
 
-	echo '<pre>';
-
+	// parse the header for pertanent information (domain names, recipient, etc)
 	$parsed = $emailHandler->parseHeader($emailHeader);
 
-	// var_dump($parsed);
-
+	// pull user from database
 	$u = $db->getUserByAddress($parsed['to']);
 	if(!$u){
 		echo 'User not found.';
 		return;
 	}
 
-	$spam = array("score"=>0, "domain"=>null, "auth"=>null);
+	// used to store repuation data for authenticated domain
+	$auth = null;
 
 	if($parsed['spf']){
 		$result = $db->getDomainReputation($parsed['spf'], 'spf');
 		if($result){
-			$spam['score'] = $result[0]['score'];
-			$spam['domain'] = $parsed['spf'];
-			$spam['auth'] = 'spf';
-			$spam['reputation'] = $result[0];
+			$auth = array();
+			$auth['score'] = $result[0]['score'];
+			$auth['domain'] = $parsed['spf'];
+			$auth['type'] = 'spf';
+			$auth['reputation'] = $result[0];
 		}
 	}
 
 	if($parsed['dkim']){
 		$result = $db->getDomainReputation($parsed['dkim'], 'dkim');
-		if($result && $result[0]['score'] > $spam){
-			$spam['score'] = $result[0]['score'];
-			$spam['domain'] = $parsed['dkim'];
-			$spam['auth'] = 'dkim';
-			$spam['reputation'] = $result[0];
+		if($result && $result[0]['score'] > $auth){
+			$auth = array();
+			$auth['score'] = $result[0]['score'];
+			$auth['domain'] = $parsed['dkim'];
+			$auth['type'] = 'dkim';
+			$auth['reputation'] = $result[0];
 		}
 	}
 
 	// if domain not authenticated...
-	if(!$spam['domain']){
+	if(!$auth){
 		echo 'Domain not authenticated';
 		return;
 	}
 
+	// leave body blank for now
 	$messageData = array(
 		"body"=>"",
 		"header"=>$emailHeader,
 		"spamScore"=>$parsed['spamScore'],
 		"isSpam"=>true,
-		"authType"=>$spam['auth'],
-		"domain"=>$spam['domain'],
+		"authType"=>$auth['type'],
+		"domain"=>$auth['domain'],
 	);
 
-	if($spam['score'] > $u['spam_threshold']){
+	if($auth['score'] > $u['spam_threshold']){
 		$messageData['isSpam'] = false;
 		echo 'Domain is good';
 	}
 
+	// calcuate the domain name's new reputation
+	$newReputation = $emailHandler->calculateReputation($auth['reputation'], $messageData['isSpam'], false);
 
-	$newReputation = $emailHandler->calculateReputation($spam['reputation'], $messageData['isSpam'], false);
-
-	// var_dump($newReputation);
+	// Update new reputation in database
 	$db->updateDomainReputation($newReputation);
 
+	// add message to database
 	$db->addNewMessage($messageData, $u['UserID']);
 
-
+	echo "New email added to database";
 ?>
